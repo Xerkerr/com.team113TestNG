d3fdec4cba6573e75f5bb2df7e4c040b66fdf735
Xerkerr <berkersaatci96@gmail.com>
2023-05-22 16:21:49 +0300

    songuncelleme

--- src/test/java/tests/day16_POM_Assertions/C01_DriverClassSonHali.java
+++ src/test/java/tests/day16_POM_Assertions/C01_DriverClassSonHali.java
@@ -3,18 +3,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
+
 public class C01_DriverClassSonHali {
+
     // 3 tane test methodu olusturalim
     // 1. method'da amazon'a gidip, url'in amazon icerdigini test edin
     @Test
     public void amazonTest(){
+
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
         String expectedIcerik ="amazon";
         String actualUrl= Driver.getDriver().getCurrentUrl();
         Assert.assertTrue(actualUrl.contains(expectedIcerik));
         Driver.closeDriver();
     }
+
     // 2. method'da Wisequarter anasayfaya gidip, title'in Wise Quarter icerdigini test edin
+
     @Test
     public void wiseTest(){
         Driver.getDriver().get(ConfigReader.getProperty("wiseUrl"));
@@ -23,6 +29,7 @@ public class C01_DriverClassSonHali {
         Assert.assertTrue(actualTitle.contains(expectedIcerik));
         Driver.closeDriver();
     }
+
     // 3. method'da facebook anasayfaya gidip, title'in Facebook icerdigini test edin
     @Test
     public void facebookTest(){
@@ -32,5 +39,6 @@ public class C01_DriverClassSonHali {
         Assert.assertTrue(actualTitle.contains(expectedIcerik));
         Driver.closeDriver();
     }
+
     // her method'da yeni driver olusturup, method sonunda driver'i kapatin
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
 import java.time.Duration;
+
 public class Driver {
     private static WebDriver driver;
     private Driver(){
@@ -17,10 +19,16 @@ public class Driver {
 
          */
     }
+
     public static WebDriver getDriver(){
+
+
         String istenenBrowser = ConfigReader.getProperty("browser");
         // chrome, firefox, safari, edge
+
+
         if(driver == null){
+
             switch (istenenBrowser){
                 case "firefox" :
                     WebDriverManager.firefoxdriver().setup();
@@ -30,6 +38,7 @@ public class Driver {
                     WebDriverManager.safaridriver().setup();
                     driver= new SafariDriver();
                     break;
+
                 case "edge" :
                     WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
@@ -37,19 +46,27 @@ public class Driver {
                 default:
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+
             }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
+
         return driver;
     }
+
+
     public static void closeDriver(){
+
         if (driver != null){
             driver.close();
             driver=null;
         }
     }
+
     public static void quitDriver(){
+
         if (driver != null){
             driver.quit();
             driver=null;
